1c62c89839d0ae9267bd0da7fbdfd6e55730312e
projekt0001i <projekt0001i@gmail.com>
2026-02-27 13:15:20 +0530

    force IPv4 and use port 587 instead of 465. Updated mailer.js

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -5,7 +5,10 @@ let transporter;
 const createTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
